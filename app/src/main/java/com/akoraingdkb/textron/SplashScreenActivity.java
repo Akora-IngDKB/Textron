f8e67c5674a2c112bc554af3ebd0484fb5623754
Akora Ing. DKB <debrahkwesibuabeng2@gmail.com>
2020-06-17 22:13:16 +0000

    Initial Commit with Splash Screen

---
+++ app/src/main/java/com/akoraingdkb/textron/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.akoraingdkb.textron;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                SplashScreenActivity.this.finish();
+            }
+        }, 2000);
+    }
+}
