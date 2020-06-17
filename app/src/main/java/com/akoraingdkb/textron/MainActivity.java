d8366ff34b897b045bf54b5475e81bdffaa4ec50
Akora Ing. DKB <debrahkwesibuabeng2@gmail.com>
2020-06-17 23:29:35 +0000

    Set up SMS function and request permission

--- app/src/main/java/com/akoraingdkb/textron/MainActivity.java
+++ app/src/main/java/com/akoraingdkb/textron/MainActivity.java
@@ -1,14 +1,64 @@
 package com.akoraingdkb.textron;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    final static int PERMISSION_REQUEST_CODE = 1122;
+    private Button btnMsg1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMsg1 = findViewById(R.id.btn_msg1);
+
+        btnMsg1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissionAndSendSMS();
+            }
+        });
+    }
+
+    private void checkPermissionAndSendSMS() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+            sendSMS();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void sendSMS() {
+        try {
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage("+233246288313", null, "Just testing some things", null, null);
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendSMS();
+            } else {
+                Toast.makeText(MainActivity.this, "The app does not have SMS permission", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
